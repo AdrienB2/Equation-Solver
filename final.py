b32f3e029ded67073e972c1acc14b5680d0291cb
JazzyJazz <83658991+JazzyJazz@users.noreply.github.com>
2022-05-06 15:17:58 +0200

    Update final.py

--- final.py
+++ final.py
@@ -8,28 +8,33 @@ from matplotlib.axis import Axis
 from numpy import e, sin, cos, tan, sqrt
 from cmath import nan
 
-
-
-### Fonctions
-
-# la fonction f 
-def f(x):
-    # Définition de la fonction ici
-    try:
-        return eval("1/x")
-    except:
-        return nan      #not a number
-
-
+# variable contenant la fonction en string
+function = ""
 
 # le h va déterminer l'ordre de précision de la dérivée
 # h = 0.000000000001 est la plus petite valeur que Python supporte
 h = 0.000000000001
+
+
+### Fonctions
+
+# la fonction f dont on cherche les zéros
+def f(x):
+    # global permet d'avoir partout la même référence à la variable "function" 
+    global function
+    # Essaie de calculer f(x) (en fonction du x)
+    try:
+        return eval(function)
+    # Si il n'y arrive pas (par exemple lors de division par 0), le code retourne un "Not a Number", ce qui permet au programme de continuer sans erreurs
+    except:
+        return nan
+
+
 # la dérivée
 def derivative(f, x):
     # utilisation de la définition de la dérivée avec la limite ; f'(a) = lim h->0 ((f(a + h) - f(a))/h)
     f1 = f(x)
-    f2 = f(x + h) 
+    f2 = f(x + h)
     fDerivee = (f2 - f1)/h  
     return fDerivee
 
